b3cb9626a86ed4ad0b5086e8be484cf9ea20518f
Semih Karadağ <80643417+semih816@users.noreply.github.com>
2022-04-18 04:22:04 +0300

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,26 @@
+ <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+    <div class="container">
+    
+      <a class="navbar-brand" href="<?php echo baseURL?>">Semih</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarCollapse">
+        <ul class="navbar-nav ms-auto mb-2 mb-md-0">
+          <li class="nav-item">
+            <a class="nav-link " aria-current="page" href="<?php echo baseURL?>">Ana Sayfa</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link " aria-current="page" href="<?php echo baseURL?>/sayfalar/hakkimizda.php">Hakkımızda</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link " aria-current="page" href="<?php echo baseURL?>/sayfalar/iletişim.php">İletişim</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link " aria-current="page" href="<?php echo baseURL?>/sayfalar/referanslar.php">Referans</a>
+          </li>
+        </ul>
+        
+      </div>
+    </div>
+  </nav>
